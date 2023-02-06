918d4ba01e0f2d9086047633d9d3f8ce15d2917c
popeye <popeye.li@poloniex.com>
2023-02-06 17:48:03 +0800

    grpc server modify

--- micro-grpc-server/src/test/java/com/autumn/component/ComponentTest.java
+++ micro-grpc-server/src/test/java/com/autumn/component/ComponentTest.java
@@ -1,8 +1,5 @@
 package com.autumn.component;
 
-import com.autumn.utils.security.JwtUtils;
-import com.autumn.vo.security.TokenUserDetails;
-import com.nimbusds.jose.jwk.RSAKey;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.security.interfaces.RSAKey;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 public class ComponentTest {
-
-    @Autowired
-    private RSAKey rsaKey;
-
-    @Test
-    public void testJWT(){
-        TokenUserDetails tokenUserDetails = new TokenUserDetails();
-        tokenUserDetails.setUserId(1);
-        tokenUserDetails.setLoginName("dacheng");
-        String s = JwtUtils.generateToken(tokenUserDetails, rsaKey);
-        System.err.println(s);
-    }
 }
--- micro-grpc-server/src/test/java/com/autumn/controller/LoginControllerTest.java
+++ micro-grpc-server/src/test/java/com/autumn/controller/LoginControllerTest.java
@@ -1,15 +1,11 @@
 package com.autumn.controller;
 
-import com.alibaba.fastjson2.JSONObject;
+import com.google.common.net.HttpHeaders;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -34,42 +30,7 @@ class LoginControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Resource
-    private PasswordEncoder passwordEncoder;
     String token;
-
-    @BeforeEach
-    public void init() throws Exception {
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.set("username", "1029842247@qq.com");
-        params.set("password", "123456");
-        MockHttpServletResponse response = mockMvc.perform(post("/login")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .params(params))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(jsonPath("status", "1").exists())
-                .andReturn().getResponse();
-        token ="Bearer " + JSONObject.parseObject(response.getContentAsString()).getJSONObject("result").getString("access_token");
-    }
-
-    @Test
-    void testHello() throws Exception {
-        mockMvc.perform(get("/api/test/v1/test")
-                        .header(HttpHeaders.AUTHORIZATION, token))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(content().string("hello"));
-    }
-
-    @Test
-    void testIndex() throws Exception {
-        mockMvc.perform(get("/index")
-                        .header(HttpHeaders.AUTHORIZATION, token))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(content().string("index"));
-    }
 }
 
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
